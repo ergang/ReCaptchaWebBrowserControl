0d9adb90be72fd550f2a87fc4afddacc3de94432
ergang <esun1@corp.intuit.net>
2016-09-24 23:05:12 -0500

    add exe and README

---
+++ README.md
@@ -0,0 +1,19 @@
+## Default setting of WPF Web Browser Control does not render Medium security preference setting of ReCaptcha 
+
+To run the program on a Windows Machine
+1. Navigate to ReCaptchaWebBrowserControl\bin\Release
+2. Run `ReCaptchaWebBrowserControl.exe`
+
+OR
+
+1. Open Visual Studio 2015 and run the program.
+
+* The low setting page is http://ergang.github.io/reCaptchaLow.html
+* The med setting page is http://ergang.github.io/reCaptchaMedium.html
+* The page is using latest render and JS engine on the machine with the meta tag `<meta http-equiv="X-UA-Compatible" content="IE=edge">`
+* However the user agent is the default with IE 7
+
+##### More Info about .NET WPF Web Browser Control:
+
+https://blogs.msdn.microsoft.com/patricka/2015/01/12/controlling-webbrowser-control-compatibility/
+
--- ReCaptchaWebBrowserControl/MainWindow.xaml.cs
+++ ReCaptchaWebBrowserControl/MainWindow.xaml.cs
@@ -41,26 +41,26 @@ namespace ReCaptchaWebBrowserControl
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            int BrowserVer, RegVal;
+            //int BrowserVer, RegVal;
 
-            BrowserVer = 8;
+            //BrowserVer = 8;
 
-            // set the appropriate IE version
-            if (BrowserVer >= 11)
-                RegVal = 11001;
-            else if (BrowserVer == 10)
-                RegVal = 10001;
-            else if (BrowserVer == 9)
-                RegVal = 9999;
-            else if (BrowserVer == 8)
-                RegVal = 8888;
-            else
-                RegVal = 7000;
+            //// set the appropriate IE version
+            //if (BrowserVer >= 11)
+            //    RegVal = 11001;
+            //else if (BrowserVer == 10)
+            //    RegVal = 10001;
+            //else if (BrowserVer == 9)
+            //    RegVal = 9999;
+            //else if (BrowserVer == 8)
+            //    RegVal = 8888;
+            //else
+            //    RegVal = 7000;
 
-            // set the actual key
-            RegistryKey Key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_BROWSER_EMULATION", true);
-            Key.SetValue(System.Diagnostics.Process.GetCurrentProcess().ProcessName + ".exe", RegVal, RegistryValueKind.DWord);
-            Key.Close();
+            //// set the actual key
+            //RegistryKey Key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_BROWSER_EMULATION", true);
+            //Key.SetValue(System.Diagnostics.Process.GetCurrentProcess().ProcessName + ".exe", RegVal, RegistryValueKind.DWord);
+            //Key.Close();
         }
     }
 }
